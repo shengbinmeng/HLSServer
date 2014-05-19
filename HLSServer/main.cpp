dff1fcfadd3cd53ccdc172deeea0694104dcccd8
lzh <darkgehom@gmail.com>
2014-05-19 22:42:28 +0800

    Initial Commit

---
+++ HLSServer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  HLSServer
+//
+//  Created by lzh on 14-5-19.
+//  Copyright (c) 2014年 lzh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
