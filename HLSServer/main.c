231f6de44af4789bea9cd05c88531c9b4d3744cb
Shengbin Meng <shengbinmeng@gmail.com>
2017-01-15 16:11:17 +0800

    Remove a line that causes error on Linux
    The sin_len field in struct sockaddr_in only exits on BSD systems.

--- HLSServer/main.c
+++ HLSServer/main.c
@@ -20,7 +20,6 @@ int create_server_socket(short port)
 {
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr = {0};
-    addr.sin_len = sizeof(addr);
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
     addr.sin_addr.s_addr = INADDR_ANY;
